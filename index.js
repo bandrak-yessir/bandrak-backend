6a69801ea44d11be8896b808c463ebef44c822a4
Bandrak-yessir <bandrakyessir@gmail.com>
2025-12-25 22:13:46 +0700

    Update files dari MT Manager

---
+++ index.js
@@ -0,0 +1,20 @@
+{
+  "name": "bandrak-backend",
+  "version": "1.0.0",
+  "description": "Backend deploy HTML",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "author": "Bandrak Senpai",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "axios": "^1.6.8",
+    "multer": "^1.4.5"
+  }
+}
+
++0
++
